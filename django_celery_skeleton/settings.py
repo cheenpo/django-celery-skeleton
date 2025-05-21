ec53a05f34286971ee380baa557472aca5c9ede2
cyrus <cheenpo@gmail.com>
2025-05-20 22:51:10 -0700

    saving state. super basic view now possible. needs formatting

--- django_celery_skeleton/settings.py
+++ django_celery_skeleton/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'django_celery_skeleton.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'django_celery_skeleton/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_celery_skeleton/views.py
+++ django_celery_skeleton/views.py
@@ -1,8 +1,9 @@
 import os
 
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
+from django.template.loader import render_to_string
 
 
 class ShowLogs(View):
@@ -12,6 +13,8 @@ class ShowLogs(View):
         for logfile in logfiles:
             logfile_s = logfile.split("-")
             logs.append({"filename": logfile, "taskname": logfile_s[0], "epoch": int(logfile_s[1].replace(".json", ""))})
+        data = {"status": "success", "logs": logs}
+        #return JsonResponse(data)
 
-        return JsonResponse({"status": "success", "logs": logs})
-        #return JsonResponse({"status": "success"})
+        HTML_STRING = render_to_string("logs.html", context=data)
+        return HttpResponse(HTML_STRING)
