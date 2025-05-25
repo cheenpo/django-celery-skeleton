82dfd27ac3d5d85d349f9b5f4c347c3ba7aac1f8
cyrus <cheenpo@gmail.com>
2025-05-24 23:41:54 -0700

    celery ing

--- django_celery_skeleton/celery.py
+++ django_celery_skeleton/celery.py
@@ -17,7 +17,8 @@ app.config_from_object("django.conf:settings", namespace="SKELETON")
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender: Celery, **kwargs):
-	sender.add_periodic_task(2.0, verify_workspace.s(), name='verify_workspace')
+	sender.add_periodic_task(10.0, verify_workspace.s(), name='verify_workspace')
+
 	#sender.add_periodic_task(1.0, test.s('periodic_task_1s'), name='every 1s')
 	#sender.add_periodic_task(5.0, test.s('periodic_task_5s'), name='every 5s')
 
@@ -52,8 +53,13 @@ def generate_log_file(taskname, contents):
 def test(arg):
 	print(arg)
 
-@app.task
+@app.task(name='verify_workspace')
 def verify_workspace():
-	validate_dir(settings.SKELETON_WORKSPACE)
-	validate_dir(settings.SKELETON_LOGS)
-	generate_log_file("verify_workspace", {"status": "complete"})
+	results = {}
+	results["validate_workspace"] = validate_dir(settings.SKELETON_WORKSPACE)
+	results["validate_workspace_logs"] = validate_dir(settings.SKELETON_LOGS)
+	generate_log_file("verify_workspace", {"status": "success", "results": results})
+
+
+##
+app.send_task('verify_workspace')
